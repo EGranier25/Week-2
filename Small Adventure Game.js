d7ffa8c6a2bc6295582a7e66d97e3c34e1cd021d
EGranier25 <emmagranier890@gmail.com>
2025-11-16 22:53:31 -0600

    Had to add comments to these files

--- Small Adventure Game.js
+++ Small Adventure Game.js
@@ -1,9 +1,9 @@
 function choosePathOne()
 {
     alert("You climbed up the ladder and found a juicy apple! Yummy!");
-}
+} /*Function for choosing the path up the tree*/
 
 function choosePathTwo()
 {
     alert("You crawled down the hole and found a shiny emerald! Lucky you!");
-}
+} /*Function for choosing the path down the hole*/
