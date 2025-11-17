f96f753f944223124ef0a81bcb108a9eccd775a8
EGranier25 <emmagranier890@gmail.com>
2025-11-16 22:48:07 -0600

    Add files via upload

---
+++ Small Adventure Game.js
@@ -0,0 +1,9 @@
+function choosePathOne()
+{
+    alert("You climbed up the ladder and found a juicy apple! Yummy!");
+}
+
+function choosePathTwo()
+{
+    alert("You crawled down the hole and found a shiny emerald! Lucky you!");
+}
