45ab6174105d1040ddad57dc32e74fad950bfd53
EGranier25 <emmagranier890@gmail.com>
2025-11-23 23:32:57 -0600

    Refactor path choice functions to update results div

--- Small Adventure Game.js
+++ Small Adventure Game.js
@@ -1,9 +1,23 @@
 function choosePathOne()
 {
-    alert("You climbed up the ladder and found a juicy apple! Yummy!");
+    var choosePathOne = 0; /*Variable for choosing path one*/
+
+    if (choosePathOne === 0) 
+        {
+            document.getElementById("results").innerHTML = "You chose to climb the tree!"; /*Displays user choice in the results div*/
+        }
+
+alert("You climbed up the ladder and found a juicy apple! Yummy!");
 } /*Function for choosing the path up the tree*/
 
 function choosePathTwo()
-{
-    alert("You crawled down the hole and found a shiny emerald! Lucky you!");
+{   
+    var choosePathTwo = 1; /*Variable for choosing path two*/
+
+    if (choosePathTwo === 1) 
+        {
+            document.getElementById("results").innerHTML = "You chose to go down the hole!"; /*Displays user choice in the results div*/
+        }
+
+alert("You crawled down the hole and found a shiny emerald! Lucky you!");
 } /*Function for choosing the path down the hole*/
